332c441a4765bdf940ee0fa47d5fe567f931f637
Seonggyu Park <seonggyu.park@brc.tamus.edu>
2023-04-27 15:02:40 -0500

    updated notebooks

--- setup.py
+++ setup.py
@@ -45,9 +45,9 @@ setup(
         "Development Status :: 4 - Beta",
         "Intended Audience :: Education",
         "Intended Audience :: Science/Research",
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         "Operating System :: Microsoft :: Windows",
         "License :: OSI Approved :: BSD License"
     ]
